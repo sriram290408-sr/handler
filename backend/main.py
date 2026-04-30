1dbf4262eaf8139cbb89140d334910c2797a041f
Sriram E <sriram290408@gmail.com>
2026-05-01 00:22:14 +0530

    fix: Cors root path

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app = FastAPI(title="Student Management API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://handler-three.vercel.app"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://handler-mc78.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
