87af76582de5bbae3de25822a9f17303fca0343f
Sriram E <sriram290408@gmail.com>
2026-05-01 11:45:21 +0530

    fix: fav-icon

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "/logo.png";
+import logo from "../assets/handler.png";
 
 export default function Sidebar({ username, onLogout }) {
   const baseClass =
