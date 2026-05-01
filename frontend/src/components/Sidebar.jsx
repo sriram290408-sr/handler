4a2fbbcaae7355973efbe7abd74a9133b9639b53
Sriram E <sriram290408@gmail.com>
2026-05-01 11:30:42 +0530

    fix: asset path

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "/logo.png";
 
 export default function Sidebar({ username, onLogout }) {
   const baseClass =
